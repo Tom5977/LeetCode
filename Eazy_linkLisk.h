b6dd6665aed9e488d3e50fcfad0de233e8c52582
Tom5977 <83262661+Tom5977@users.noreply.github.com>
2023-01-11 21:56:04 +0800

    1-11: easy_linklist is done

--- Eazy_linkLisk.h
+++ Eazy_linkLisk.h
@@ -69,4 +69,73 @@ public:
 		Head->next = q;
 		return Head->next;
 	}
+
+	ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+		ListNode* head = new ListNode();
+		ListNode* r = head;
+		head->next = NULL;
+		while (list1 != NULL && list2 != NULL)
+		{
+			if (list1->val < list2->val)
+			{
+				ListNode* tmp = list1->next;
+				r->next = list1;
+				list1->next = NULL;
+				r = list1;
+				list1 = tmp;
+			}
+			else
+			{
+				ListNode* tmp = list2->next;
+				r->next = list2;
+				list2->next = NULL;
+				r = list2;
+				list2 = tmp;
+			}
+		}
+		if (list1 == NULL)
+		{
+			r->next = list2;
+		}
+		else
+		{
+			r->next = list1;
+		}
+		return head->next;
+	}
+
+	bool isPalindrome(ListNode* head) {
+		vector<int> copy;
+		while (head != NULL)
+		{
+			copy.push_back(head->val);
+			head = head->next;
+		}
+		for (int i = 0; i < copy.size() / 2; i++)
+		{
+			if (copy[i] != copy[copy.size() - 1 - i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool hasCycle(ListNode* head) {
+		if (head == NULL)
+		{
+			return false;
+		}
+		ListNode* p = head;
+		while (p->next != NULL && p->next->next != NULL)
+		{
+			p = p->next->next;
+			head = head->next;
+			if (p == head)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 };
