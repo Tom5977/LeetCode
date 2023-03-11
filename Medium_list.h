69200db137f4fe305531bb2751ac0592e00a7ff2
Tom5977 <83262661+Tom5977@users.noreply.github.com>
2023-03-11 10:41:49 +0800

    longestPalindrome

--- Medium_list.h
+++ Medium_list.h
@@ -125,6 +125,37 @@ public:
 	}
 
 	string longestPalindrome(string s) {
-		return s;
+		int max = 1, pos = 0;
+		int** dp = new int* [s.size()]();
+		for (int i = 0; i < s.size(); i++) {
+			dp[i] = new int[s.size()]();
+		}
+		for (int i = 0; i < s.size(); i++) {
+			dp[i][i] = 1;
+			if (i < s.size() - 1) {
+				if (s[i] == s[i + 1]) {
+					dp[i][i + 1] = 1;
+					max = 2;
+					pos = i;
+				}
+			}
+		}
+
+		for (int l = 3; l <= s.size(); l++) {
+			for (int i = 0; i + l - 1 < s.size(); i++) {
+				int j = i + l - 1;
+				if (s[i] == s[j] && dp[i + 1][j - 1] == 1) {
+					max = l;
+					pos = i;
+					dp[i][j] = 1;
+				}
+			}
+		}
+		for (int i = 0; i < s.size(); i++)
+		{
+			delete[] dp[i];
+		}
+		delete[]dp;
+		return s.substr(pos, max);
 	}
 };
